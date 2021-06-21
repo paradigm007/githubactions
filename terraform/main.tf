ddf3fb6edaebedcddeb142e45458e13b194a220c
tejit <shahtejit@gmail.com>
2021-06-21 10:44:09 +0530

    removed comment in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,6 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 
-#   tags = {
-#     environment = "production"
-#   }
 }
 
 # Create virtual network
