2db41a050f9fc01bc1f5ab7c8b0bf8a5b964fa73
tejit <shahtejit@gmail.com>
2021-06-25 23:14:14 +0530

    added file

---
+++ script2.ps1
@@ -0,0 +1,7 @@
+Write-Host "Hello World"
+git config 
+Write-Host "GIT status"
+git status
+Write-Output "Printing Webhook Github ...";
+#Write-Output "Printing Webhook Action TEXT ${{ github.event.client_payload.text }} ...";
+#$GITHUB_CONTEXT
